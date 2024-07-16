d5f61ed3f91a3c5f824ff78146eb22c732329690
Subash Chand Thakuri <subashthakuri799@gmail.com>
2024-07-17 00:08:53 +0545

    testing console app and webapi

---
+++ consoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, from the different World!");
+string name = Console.ReadLine();
+Console.WriteLine("Name:" + name);
